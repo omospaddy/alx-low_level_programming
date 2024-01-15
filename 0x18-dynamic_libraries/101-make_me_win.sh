1c48b147bdb1a02703062bbb9937628196b13ad7
omospaddy <omospaddy@gmail.com>
2024-01-15 02:07:38 -0800

    done oo

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/omospaddy/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/omospaddy/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
